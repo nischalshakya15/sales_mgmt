c4b6f468d320836a7d7d07a61312c2b9846d494c
nischalshakya15 <nischalshakya95@gmail.com>
2020-08-20 09:49:18 +0545

    Added interceptors in Dio

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:sales_mgmt/org/personal/salemgmt/interceptors/app_interceptors.dart';
+import 'package:sales_mgmt/org/personal/salemgmt/utils/dio_utils.dart';
 import 'package:sales_mgmt/salesmgmt.dart';
 
 import 'org/personal/salemgmt/utils/env_util.dart';
 
 void main() async {
   await loadEnvFile("assets/env/.env_dev");
+  authorizationDio.interceptors.add(AppInterceptors());
   runApp(SalesMgmt());
 }
--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:sales_mgmt/org/personal/salemgmt/interceptors/app_interceptors.dart';
+import 'package:sales_mgmt/org/personal/salemgmt/utils/dio_utils.dart';
 import 'package:sales_mgmt/org/personal/salemgmt/utils/env_util.dart';
 import 'package:sales_mgmt/salesmgmt.dart';
 
 void main() async {
   await loadEnvFile("assets/env/.env_prod");
+  authorizationDio.interceptors.add(AppInterceptors());
   runApp(SalesMgmt());
 }
